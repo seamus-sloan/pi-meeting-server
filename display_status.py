0e19be26d5ef21e7fefb698d19d35420f42e91bc
Seamus Sloan <ssloan@riis.com>
2025-04-13 12:25:20 -0400

    add text to screen

--- display_status.py
+++ display_status.py
@@ -6,18 +6,36 @@ import time
 SERVER_URL = "http://localhost:5000/status"
 POLL_INTERVAL = 5 # seconds
 
-# Initialize
+# Colors
+RED = (255, 0 , 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (255, 255, 255)
+WHITE = (0, 0, 0)
+
+# Setup the screen
 pygame.init()
 screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 pygame.display.set_caption("Status Display")
 clock = pygame.time.Clock()
 
+# Setup the font
+pygame.font.init()
+font = pygame.font.SysFont("Arial", 100)  # (family, size)
+
+def draw_text(text, color):
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect(center = (screen.get_width() / 2, screen.get_height() / 2))
+    screen.blit(text_surface, text_rect)
+
 def show_red_screen():
     screen.fill((255, 0, 0))
+    draw_text("IN MEETING!", WHITE)
     pygame.display.flip()
 
 def show_green_screen():
     screen.fill((0, 255, 0))
+    draw_text("FREE!", BLACK)
     pygame.display.flip()
 
 # Main Loop
