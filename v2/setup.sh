ac68d37bb3a01caf9c409ff4b0d5b4adc79fc215
seamus <seamus.sloan@gmail.com>
2025-04-17 11:35:12 -0400

    pin tauri version and use GTK

--- v2/setup.sh
+++ v2/setup.sh
@@ -11,7 +11,8 @@ sudo apt install -y \
   cmake \
   pkg-config \
   libx11-dev \
-  libxdo-dev
+  libxdo-dev \
+  libsoup-3.0-dev \
 
 # Install rust
 curl https://sh.rustup.rs -sSf | sh
@@ -22,3 +23,6 @@ rustup target add armv7-unknown-linux-gnu
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install -y nodejs
 npm install -g pnpm
+
+# Export the PGK_CONFIG_PATH
+export PKG_CONFIG_PATH=/usr/lib/pkgconfig:/usr/share/pkgconfig
